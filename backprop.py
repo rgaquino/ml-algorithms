bd223701da160875bef40474fedcbfee60b10b93
RG Aquino <ralphgregoraquino@gmail.com>
2018-07-20 22:05:49 +0800

    [backprop] Python implementation

---
+++ backprop.py
@@ -0,0 +1,11 @@
+from random import random
+
+
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
